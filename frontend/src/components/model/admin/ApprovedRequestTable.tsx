d78723f8b7da5a0365748cb894edd939327975fc
RyunosukeSakurada <longzhizhu513@gmail.com>
2023-09-15 21:48:23 -0700

    [added]admin/店舗承認申請レスポンシブ

--- frontend/src/components/model/admin/ApprovedRequestTable.tsx
+++ frontend/src/components/model/admin/ApprovedRequestTable.tsx
@@ -151,65 +151,67 @@ const ApprovedRequestTable = () => {
           <h1 className="bold text-zinc-500">承認申請をしている店舗ユーザーが存在しません</h1>
         </div>
       ) : ( 
-        <table className="min-w-full divide-y divide-gray-200 mt-8">
-          <thead>
-            <tr className="text-center">
-              <th className="px-6 py-3 text-xs font-medium tracking-wider text-gray-500 uppercase">店舗名</th>
-              <th className="px-6 py-3 text-xs font-medium tracking-wider text-gray-500 uppercase">住所</th>
-              <th className="px-6 py-3 text-xs font-medium tracking-wider text-gray-500 uppercase">メールアドレス</th>
-              <th className="px-6 py-3 text-xs font-medium tracking-wider text-gray-500 uppercase">一時利用停止</th>
-              <th className="px-6 py-3 text-xs font-medium tracking-wider text-gray-500 uppercase">ブロック</th>
-            </tr>
-          </thead>
-          <tbody className="divide-ydivide-gray-200 text-center text-[12px]">
-            {stores.map(store => (
-              <tr key={store._id}>
-                <td className="px-6 py-4 whitespace-nowrap">{store.storeName}</td>
-                <td className="px-6 py-4 whitespace-nowrap">{store.address} {store.detailedAddress}</td>
-                <td className="px-6 py-4 whitespace-nowrap">{store.email}</td>
-                <td className="px-6 py-4 whitespace-nowrap">{store.suspended.toString()}</td>
-                <td className="px-6 py-4 whitespace-nowrap">{store.blocked.toString()}</td>
-                <td className="px-6 py-4 whitespace-nowrap">
-                  <button className="text-green-500" onClick={() => showApprovalConfirmation(store._id)}>許可</button>
-                </td>
-                <td className="px-6 py-4 whitespace-nowrap">
-                  <button onClick={() => showDeclineConfirmation(store._id)}  className="text-red-500 hover:text-red-700">却下</button>
-                </td>
+        <div className="overflow-x-auto">
+          <table className="min-w-full divide-y divide-gray-200 mt-8">
+            <thead>
+              <tr className="text-center">
+                <th className="px-12 md:px-6 py-3 text-xs font-medium tracking-wider text-gray-500 uppercase">店舗名</th>
+                <th className="px-12 md:px-6 py-3 text-xs font-medium tracking-wider text-gray-500 uppercase">住所</th>
+                <th className="px-12 md:px-6 py-3 text-xs font-medium tracking-wider text-gray-500 uppercase">メールアドレス</th>
+                <th className="px-12 md:px-6 py-3 text-xs font-medium tracking-wider text-gray-500 uppercase">一時利用停止</th>
+                <th className="px-12 md:px-6 py-3 text-xs font-medium tracking-wider text-gray-500 uppercase">ブロック</th>
               </tr>
-            ))}
-                {isApproveConfirmOpen && (
-                  <div className="fixed top-0 left-0 w-full h-full bg-gray-700 bg-opacity-50 flex items-center justify-center">
-                    <div className="bg-white p-8 rounded-lg shadow-lg text-start relative w-[400px] break-words">
-                      <h3 className="mb-4 text-center">本当に許可しますか？</h3>
-                      <div className="flex justify-center items-center">
-                        <button className="bg-green-500 text-white px-4 py-2 rounded-lg mt-4 mr-2" onClick={handleConfirmApprove}>
-                          許可する
-                        </button>
-                        <button className="border text-gray-700 px-4 py-2 rounded-lg mt-4 ml-2" onClick={() => setIsApproveConfirmOpen(false)}>
-                          戻る
-                        </button>
+            </thead>
+            <tbody className="divide-ydivide-gray-200 text-center text-[12px]">
+              {stores.map(store => (
+                <tr key={store._id}>
+                  <td className="px-12 md:px-6 py-4 whitespace-nowrap">{store.storeName}</td>
+                  <td className="px-12 md:px-6 py-4 whitespace-nowrap">{store.address} {store.detailedAddress}</td>
+                  <td className="px-12 md:px-6 py-4 whitespace-nowrap">{store.email}</td>
+                  <td className="px-12 md:px-6 py-4 whitespace-nowrap">{store.suspended.toString()}</td>
+                  <td className="px-12 md:px-6 py-4 whitespace-nowrap">{store.blocked.toString()}</td>
+                  <td className="px-12 md:px-6 py-4 whitespace-nowrap">
+                    <button className="text-green-500" onClick={() => showApprovalConfirmation(store._id)}>許可</button>
+                  </td>
+                  <td className="px-12 md:px-6 py-4 whitespace-nowrap">
+                    <button onClick={() => showDeclineConfirmation(store._id)}  className="text-red-500 hover:text-red-700">却下</button>
+                  </td>
+                </tr>
+              ))}
+                  {isApproveConfirmOpen && (
+                    <div className="fixed top-0 left-0 w-full h-full bg-gray-700 bg-opacity-50 flex items-center justify-center">
+                      <div className="bg-white p-8 rounded-lg shadow-lg text-start relative w-[300px] md:w-[400px] break-words">
+                        <h3 className="mb-4 text-center">本当に許可しますか？</h3>
+                        <div className="flex justify-center items-center">
+                          <button className="bg-green-500 text-white px-4 py-2 rounded-lg mt-4 mr-2" onClick={handleConfirmApprove}>
+                            許可する
+                          </button>
+                          <button className="border text-gray-700 px-4 py-2 rounded-lg mt-4 ml-2" onClick={() => setIsApproveConfirmOpen(false)}>
+                            戻る
+                          </button>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                )}
+                  )}
 
-                {isDeclineConfirmOpen && (
-                  <div className="fixed top-0 left-0 w-full h-full bg-gray-700 bg-opacity-50 flex items-center justify-center">
-                    <div className="bg-white p-8 rounded-lg shadow-lg text-start relative w-[400px] break-words">
-                      <h3 className="mb-4 text-center">本当に却下しますか？</h3>
-                      <div className="flex justify-center items-center">
-                        <button className="bg-red-500 text-white px-4 py-2 rounded-lg mt-4 mr-2" onClick={handleConfirmDecline}>
-                          却下する
-                        </button>
-                        <button className="border text-gray-700 px-4 py-2 rounded-lg mt-4 ml-2" onClick={() => setIsDeclineConfirmOpen(false)}>
-                          戻る
-                        </button>
+                  {isDeclineConfirmOpen && (
+                    <div className="fixed top-0 left-0 w-full h-full bg-gray-700 bg-opacity-50 flex items-center justify-center">
+                      <div className="bg-white p-8 rounded-lg shadow-lg text-start relative w-[300px] md:w-[400px] break-words">
+                        <h3 className="mb-4 text-center">本当に却下しますか？</h3>
+                        <div className="flex justify-center items-center">
+                          <button className="bg-red-500 text-white px-4 py-2 rounded-lg mt-4 mr-2" onClick={handleConfirmDecline}>
+                            却下する
+                          </button>
+                          <button className="border text-gray-700 px-4 py-2 rounded-lg mt-4 ml-2" onClick={() => setIsDeclineConfirmOpen(false)}>
+                            戻る
+                          </button>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                )}
-          </tbody>
-        </table>
+                  )}
+            </tbody>
+          </table>
+        </div>
       )}
     </div>
   );
