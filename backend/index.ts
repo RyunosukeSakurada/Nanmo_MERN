4705004a42db46597b09432aea0b936af46865eb
RyunosukeSakurada <longzhizhu513@gmail.com>
2023-10-05 15:24:26 -0700

    test back on vercel front on local

--- backend/index.ts
+++ backend/index.ts
@@ -23,25 +23,24 @@ mongoose
     console.log(error.message)
   })
 
-// app.use((req:Request, res:Response, next:NextFunction) => {
-//   const allowedOrigins = [process.env.ORIGIN_URL]; 
-//   const origin = req.headers.origin;
-//   if (allowedOrigins.includes(origin)) {
-//     cors({ origin: true, credentials: true })(req, res, next);
-//   } else {
-//     cors({ origin: false, credentials: true })(req, res, next);
-//   }
-// });
-
 app.use((req:Request, res:Response, next:NextFunction) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  next();
+  const allowedOrigins = [process.env.ORIGIN_URL , "http://localhost:5173" ]; 
+  const origin = req.headers.origin;
+  if (allowedOrigins.includes(origin)) {
+    cors({ origin: true, credentials: true })(req, res, next);
+  } else {
+    cors({ origin: false, credentials: true })(req, res, next);
+  }
 });
 
+// app.use((req:Request, res:Response, next:NextFunction) => {
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   next();
+// });
 
 
 app.use(express.json())
