e1119cdf3e57f2fdd85693263b69e9d0e8420bc4
RyunosukeSakurada <longzhizhu513@gmail.com>
2023-10-05 15:52:22 -0700

    test cors

--- backend/index.ts
+++ backend/index.ts
@@ -44,7 +44,7 @@ mongoose
 
 app.use(cors());
 app.use(function (req:Request, res:Response, next:NextFunction) {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:5173');
+  res.setHeader('Access-Control-Allow-Origin', "https://nanmo-mern-frontend.vercel.app");
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
   res.setHeader('Access-Control-Allow-Credentials', 'true');
